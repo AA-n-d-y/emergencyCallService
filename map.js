b2b945bff40c2615472a585fde16c3a66e194d8c
Ryan F <103477401+powerskater@users.noreply.github.com>
2024-11-22 05:06:33 +0000

    Map popup

--- map.js
+++ map.js
@@ -1,5 +1,17 @@
+// Display map
 var map = L.map('map').setView([49.276292, -122.92], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+// Set incident location by clicking on map
+var popup = L.popup();
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("Set the incident location to " + e.latlng.toString())
+        .openOn(map);
+
+}
+map.on('click', onMapClick);
