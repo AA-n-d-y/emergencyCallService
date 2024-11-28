48d469830597e3112f7b5786f5e5141741130948
Ryan F <103477401+powerskater@users.noreply.github.com>
2024-11-28 06:28:54 +0000

    Map popup

--- map.js
+++ map.js
@@ -1,17 +1,60 @@
+const accessToken = "AAPTxy8BH1VEsoebNVZXo8HurGWUPfpCnqmtiYOporIn6aReHTWuMzBwJj870WY7-50fnNywKgwy55pAudeX1Fp_erFecK20XrulPEKH08QchSg2tb1wETJwd8rqQ7aLU_aEWeKN7a28QLrXGKoqgYfX2XzTAZcdL1G_tBXuDdSuw-9cAcCZ76mw2IXcXLIdZzBq8chznVeyHaroHSB5xCmVzv_IUb-Hka5OnuMtf5wv7e8.AT1_KKIj9iLd";
+const basemapEnum = "arcgis/navigation";
 // Display map
-var map = L.map('map').setView([49.276292, -122.92], 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+const map = L.map("map", {
+    minZoom: 2
+  })
+
+  map.setView([49.276292, -122.92], 13); // Paris
+
+  L.esri.Vector.vectorBasemapLayer(basemapEnum, {
+    token: accessToken
+  }).addTo(map);
+
 
 // Set incident location by clicking on map
-var popup = L.popup();
-function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("Set the incident location to " + e.latlng.toString())
-        .openOn(map);
+const layerGroup = L.layerGroup().addTo(map);
+var coordinates = 0;
+var addressStr = "";
+var addressBox = document.querySelector("#address");
 
+map.on("click", function (e) {
+
+    L.esri.Geocoding
+      .reverseGeocode({
+        apikey: accessToken
+      })
+      .latlng(e.latlng)
+
+      .run(function (error, result) {
+        if (error) {
+          return;
+        }
+
+        layerGroup.clearLayers();
+
+        marker = L.marker(result.latlng).addTo(layerGroup);
+
+        const lngLatString = `${Math.round(result.latlng.lng * 100000) / 100000}, ${Math.round(result.latlng.lat * 100000) / 100000}`;
+
+        marker.bindPopup(`<strong>${lngLatString}</strong><p>${result.address.Match_addr}</p>`);
+        marker.openPopup();
+
+        coordinates = result.latlng;
+        addressStr = result.address.Match_addr;
+        addressBox.value = result.address.Match_addr;
+        
+          
+      });
+  });
+
+// Add marker onto map after submitting form
+function addMarker(){
+    var marker = L.marker(coordinates).addTo(map);
+    marker.bindPopup(`<strong>${addressStr}</strong><p>Type: </p>`);
 }
-map.on('click', onMapClick);
+
+
+
+
+
